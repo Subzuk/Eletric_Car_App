2ddf4836eaa4cb106fff331acc44840af663de7c
Subzuk <gustavo.poleszuk@gmail.com>
2023-08-28 14:16:02 -0300

    Criação do App

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Eletrci_Car_APP"
+include(":app")
+ 
